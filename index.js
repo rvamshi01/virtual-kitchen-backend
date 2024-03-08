acb5a4580a285ac04737a5874f30ccf289734d8a
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-08 09:44:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/virtual-Kitchen', {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
 });
-const db = mongoose.connection
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // Define routes
