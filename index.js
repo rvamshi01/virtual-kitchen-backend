95e5ce6902d15084a0cbea7a9e4934c3d5117717
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-19 10:42:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 // Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/virtual-Kitchen', {
