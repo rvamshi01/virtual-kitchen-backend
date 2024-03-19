d837ef2da53b2a86c603cc1fe622f6e1c5a3dbee
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-19 10:42:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const app = express();
+const app = express()
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
