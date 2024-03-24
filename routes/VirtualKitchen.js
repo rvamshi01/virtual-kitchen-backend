13c6b13197d2f2ecf598b3a002d676a52487036f
Rachakonda.Vamshi <146413004+rvamshi01@users.noreply.github.com>
2024-03-24 10:48:03 +0530

    Update VirtualKitchen.js

--- routes/VirtualKitchen.js
+++ routes/VirtualKitchen.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const VirtualKitchen = require('../models/VirtualKitchen');
 
 // Get all virtual kitchens
-router.get('/get', async (req, res) => {
+router.get('/ge', async (req, res) => {
     try {
         const virtualKitchens = await VirtualKitchen.find()
         res.json(virtualKitchens);
